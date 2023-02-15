21c24498c16a0e51dc38225b2bf1cda26ecd3df3
SakthivelMurugan <sakthi255vel@gmail.com>
2023-02-16 04:00:26 +0530

    adding task5

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask,render_template,request
+
+
+app=Flask(__name__)
+
+
+lis=[]
+@app.route("/create")
+def ecommerce():
+    lis.append(stu)
+    return render_template("index.html",data=lis)
+
+
+class buyer:
+    def __init__(self, name, age,purchased,amount,url):
+        self.name=name
+        self.age=age
+        self.purchased=purchased
+        self.amount=amount
+        self.url=url
+
+    def product(self):
+        return self.name,self.age,self.purchased,self.amount,self.url
+
+
+@app.route("/",methods=['get','post'])
+def create_object():
+    if request.method == "POST":
+        name=request.form.get("name")
+        age=request.form.get("age")
+        purchased=request.form.get("purchased")
+        amount=request.form.get("amount")
+        url=request.form.get("url")
+        obj=buyer(name,age,purchased,amount,url)
+        lis.append(obj)
+    return render_template("create.html",data=lis)
+        
+    
+stu=buyer("sakthi",22,"mobile",25000,"/static/images/mobile.png")
+
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
